c07a7d2048018bd0a531f0bece9a64ff4d6f65c2
Truc Tran <tracytrle@gmail.com>
2023-06-21 10:33:09 -0500

    first React project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+// rfce
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(0);
+  let color = count > 0 ? "green" : count < 0 ? "red" : "black";
+
+  // if (count > 0) {
+  //   color = "green";
+  // }
+  // if (count < 0) {
+  //   color = "red";
+  // }
+  // if (count === 0) {
+  //   color = "black";
+  // }
+  const increment = () => setCount(count + 1);
+  const decrement = () => setCount(count - 1);
+  const resetCount = () => setCount(0);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="container">
+      <h1>Counter</h1>
+      <span style={{ color }} id="value">
+        {count}
+      </span>
+      <div class="btn-group">
+        <button onClick={decrement} class="btn btn-decrease">
+          DECREASE
+        </button>
+        <button onClick={resetCount} class="btn btn-reset">
+          RESET
+        </button>
+        <button onClick={increment} class="btn btn-increase">
+          INCREASE
+        </button>
+      </div>
     </div>
   );
 }
